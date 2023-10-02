82b925ef1f017f6b1a06990fbc7d4a7fff8ac2fb
NowWeAre-Free <kontodarm@wp.pl>
2023-10-02 20:39:22 +0200

    add some content + styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
-export const Home = () => {
-  return <div>Home</div>;
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      <section className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Marcin Lichota
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Frontend Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I'm a frontend developer. Currently, I'm focused on builidng resposive
+          web applications.
+        </p>
+        <div>
+          <button>
+            View Projects
+            <HiArrowNarrowRight />
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 };
+
+export default Home;
