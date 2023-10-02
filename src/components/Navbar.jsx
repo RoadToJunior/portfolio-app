f8b03ee9c05702b18daa603b3e3a9ce37725c393
NowWeAre-Free <kontodarm@wp.pl>
2023-10-02 18:46:20 +0200

    bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import Logo from "../assets/logo.png";
 
 const Navbar = () => {
+  const [nav, SetNav] = useState(false);
+  const handleClick = () => SetNav(!nav);
+
   return (
     <navbar className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <section>
-        <img src={Logo} alt="Logo" style={{ width: "200px" }} />
+        <p>M.L - RoadToJunior</p>
       </section>
 
       {/* menu */}
@@ -18,15 +20,26 @@ const Navbar = () => {
         <li>Work</li>
         <li>Contact</li>
       </ul>
-
       {/* hamburger */}
-      <section className="hidden">
-        <FaBars />
+      <section onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
       </section>
       {/* mobile menu */}
-      <section></section>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
       {/* social icons */}
-      <section></section>
+      <section className="hidden"></section>
     </navbar>
   );
 };
