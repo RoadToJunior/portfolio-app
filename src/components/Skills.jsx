29df881bcf68c0a8fbaf3fc042c576ae38b218f3
NowWeAre-Free <kontodarm@wp.pl>
2023-10-05 18:04:53 +0200

    connect logos to links, add target blank, rounded corners

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -22,38 +22,64 @@ const Skills = () => {
 
         {/* container for all icons */}
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={HTML} alt="HTML logo" />
-            <p className="my-4">HTML</p>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/HTML?retiredLocale=pl"
+              target="_blank"
+            >
+              <img className="w-20 mx-auto" src={HTML} alt="HTML logo" />
+              <p className="my-4">HTML</p>
+            </a>
           </div>
 
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind logo" />
-            <p className="my-4">Tailwind</p>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
+            <a href="https://tailwindcss.com/" target="_blank">
+              <img
+                className="w-20 mx-auto"
+                src={Tailwind}
+                alt="Tailwind logo"
+              />
+              <p className="my-4">Tailwind</p>
+            </a>
           </div>
 
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 py-5 mx-auto" src={Sass} alt="Sass logo" />
-            <p className="my-4">Sass</p>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
+            <a href="https://sass-lang.com/" target="_blank">
+              <img className="w-20 py-5 mx-auto" src={Sass} alt="Sass logo" />
+              <p className="my-4">Sass</p>
+            </a>
           </div>
 
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Github} alt="GitHub logo" />
-            <p className="my-4">GitHub</p>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
+            <a href="https://github.com/RoadToJunior" target="_blank">
+              <img className="w-20 mx-auto" src={Github} alt="GitHub logo" />
+              <p className="my-4">GitHub</p>
+            </a>
           </div>
 
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={ReactLogo} alt="React.js logo" />
-            <p className="my-4">React</p>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
+            <a href="https://react.dev/" target="_blank">
+              <img
+                className="w-20 mx-auto"
+                src={ReactLogo}
+                alt="React.js logo"
+              />
+              <p className="my-4">React</p>
+            </a>
           </div>
 
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-20 mx-auto"
-              src={Javascript}
-              alt="Javascript logo"
-            />
-            <p className="my-4">Javascript</p>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript?retiredLocale=pl"
+              target="_blank"
+            >
+              <img
+                className="w-20 mx-auto"
+                src={Javascript}
+                alt="Javascript logo"
+              />
+              <p className="my-4">Javascript</p>
+            </a>
           </div>
         </div>
       </div>
