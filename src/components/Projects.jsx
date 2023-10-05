6b9bb60a8d2a2616121f41c0afe534048c120164
NowWeAre-Free <kontodarm@wp.pl>
2023-10-05 21:28:17 +0200

    add html structure

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section
+      name="projects"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
+    >
+      <div>
+        <div>
+          <p>Projects</p>
+          <p>// Check out some of my recent projects</p>
+        </div>
+
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
