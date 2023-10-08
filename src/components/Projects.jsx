5d78382c79f04b507a98d19508aeceb014647ab7
NowWeAre-Free <kontodarm@wp.pl>
2023-10-08 18:34:46 +0200

    add html structure + style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ProjectImg from "../assets/workImg.jpeg";
 
 const Projects = () => {
   return (
@@ -6,14 +7,37 @@ const Projects = () => {
       name="projects"
       className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
     >
-      <div>
-        <div>
-          <p>Projects</p>
-          <p>// Check out some of my recent projects</p>
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto p4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            projects( )
+          </p>
+          <p className="py-6">// Check out some of my recent projects</p>
         </div>
 
-        <div>
-          <div></div>
+        <div
+          style={{
+            backgroundImage: `url(${ProjectImg})`,
+          }}
+          className="content-div grid sm:grid-cols-2 md:grid-cols-3 gap-4"
+        >
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto">
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React.js app
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button>Demo</button>
+                </a>
+                <a href="/">
+                  <button>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
