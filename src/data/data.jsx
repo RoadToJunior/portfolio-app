0a9ced2295e36160c5dd9dd40e7a63699ae7e281
JSisLove <kontodarm@wp.pl>
2023-10-13 19:49:21 +0200

    :tada: init

---
+++ src/data/data.jsx
@@ -0,0 +1,30 @@
+import ProjectIMG from '../assets/workImg.jpeg';
+
+export const data = [
+    {
+        id:1,
+        name: "React JS Application",
+        image: ProjectIMG,
+        github: "",
+        live: "",        
+    },    {
+        id:1,
+        name: "React JS Application",
+        image: ProjectIMG,
+        github: "",
+        live: "",        
+    },    {
+        id:1,
+        name: "React JS Application",
+        image: ProjectIMG,
+        github: "",
+        live: "",        
+    },   
+     {
+        id:1,
+        name: "React JS Application",
+        image: ProjectIMG,
+        github: "",
+        live: "",        
+    }
+]
