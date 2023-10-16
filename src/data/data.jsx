50af16e76a5c0be00c18f29b05652281f5f9eef5
JSisLove <kontodarm@wp.pl>
2023-10-16 18:09:36 +0200

    add navbar objects list

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,30 +1,33 @@
-import ProjectIMG from '../assets/workImg.jpeg';
+import ProjectIMG from "../assets/workImg.jpeg";
 
 export const data = [
-    {
-        id:1,
-        name: "React JS Application",
-        image: ProjectIMG,
-        github: "",
-        live: "",        
-    },    {
-        id:1,
-        name: "React JS Application",
-        image: ProjectIMG,
-        github: "",
-        live: "",        
-    },    {
-        id:1,
-        name: "React JS Application",
-        image: ProjectIMG,
-        github: "",
-        live: "",        
-    },   
-     {
-        id:1,
-        name: "React JS Application",
-        image: ProjectIMG,
-        github: "",
-        live: "",        
-    }
-]
+  {
+    id: 1,
+    name: "Portfolio website",
+    image: ProjectIMG,
+    github: "https://github.com/RoadToJunior/portfolio-app",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "alelipaco.com ecommerce",
+    image: ProjectIMG,
+    github: "https://github.com/RoadToJunior/alelipa",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "test",
+    image: ProjectIMG,
+    github: "",
+    live: "",
+  },
+];
+
+export const navbarList = [
+  { text: "Home", to: "home" },
+  { text: "About", to: "about" },
+  { text: "Projects", to: "projects" },
+  { text: "Contact", to: "contact" },
+  { text: "GitHub", href: "https://github.com/RoadToJunior" },
+];
