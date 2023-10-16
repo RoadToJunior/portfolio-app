37b1a7ac9452a2b193a24a2f6d9f3f5d419bfd25
JSisLove <kontodarm@wp.pl>
2023-10-16 18:14:54 +0200

    add skills objects list

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,4 +1,10 @@
 import ProjectIMG from "../assets/workImg.jpeg";
+import Tailwind from "../assets/tailwind.png";
+import HTML from "../assets/html.png";
+import Sass from "../assets/sass-1-logo-png-transparent.png";
+import Github from "../assets/github.png";
+import ReactLogo from "../assets/react.png";
+import Javascript from "../assets/javascript.png";
 
 export const data = [
   {
@@ -30,4 +36,43 @@ export const navbarList = [
   { text: "Projects", to: "projects" },
   { text: "Contact", to: "contact" },
   { text: "GitHub", href: "https://github.com/RoadToJunior" },
+];
+
+export const skillsList = [
+  {
+    name: "HTML",
+    src: HTML,
+    alt: "HTML Logo",
+    href: "https://developer.mozilla.org/en-US/docs/Web/HTML?retiredLocale=pl",
+  },
+  {
+    name: "Tailwind",
+    src: Tailwind,
+    alt: "Tailwind Logo",
+    href: "https://tailwindcss.com/",
+  },
+  {
+    name: "Sass",
+    src: Sass,
+    alt: "Sass Logo",
+    href: "https://sass-lang.com/",
+  },
+  {
+    name: "GitHub",
+    src: Github,
+    alt: "GitHub Logo",
+    href: "https://github.com/RoadToJunior",
+  },
+  {
+    name: "React",
+    src: ReactLogo,
+    alt: "React Logo",
+    href: "https://react.dev/",
+  },
+  {
+    name: "JavaScript",
+    src: Javascript,
+    alt: "JavaScript Logo",
+    href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript?retiredLocale=pl",
+  },
 ];
