efef0538246bf2af5af66ace0cb8c353edf23344
JSisLove <kontodarm@wp.pl>
2023-10-16 18:37:07 +0200

    cleaned up code using map method on skills list

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -19,71 +19,14 @@ const Skills = () => {
 
         {/* container for all icons */}
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8">
-          <div className="shadow-2xl shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
-            <a
-              href="https://developer.mozilla.org/en-US/docs/Web/HTML?retiredLocale=eng"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img className="w-20 mx-auto" src={HTML} alt="HTML logo" />
-              <p className="my-4">HTML</p>
-            </a>
-          </div>
-
-          <div className="shadow-2xl shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
-            <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
-              <img
-                className="w-20 mx-auto"
-                src={Tailwind}
-                alt="Tailwind logo"
-              />
-              <p className="my-4">Tailwind</p>
-            </a>
-          </div>
-
-          <div className="shadow-2xl shadow-[#040c16]  hover:scale-110 duration-500 rounded-3xl">
-            <a href="https://sass-lang.com/" target="_blank" rel="noreferrer">
-              <img className="w-20 py-2 mx-auto" src={Sass} alt="Sass logo" />
-              <p className="my-4">Sass</p>
-            </a>
-          </div>
-
-          <div className="shadow-2xl shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
-            <a
-              href="https://github.com/RoadToJunior"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img className="w-20 mx-auto" src={Github} alt="GitHub logo" />
-              <p className="my-4">GitHub</p>
-            </a>
-          </div>
-
-          <div className="shadow-2xl shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
-            <a href="https://react.dev/" target="_blank" rel="noreferrer">
-              <img
-                className="w-20 mx-auto"
-                src={ReactLogo}
-                alt="React.js logo"
-              />
-              <p className="my-4">React</p>
-            </a>
-          </div>
-
-          <div className="shadow-2xl shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
-            <a
-              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript?retiredLocale=pl"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                className="w-20 mx-auto"
-                src={Javascript}
-                alt="Javascript logo"
-              />
-              <p className="my-4">Javascript</p>
-            </a>
-          </div>
+          {skillsList.map((item) => (
+            <div className="shadow-2xl shadow-[#040c16] hover:scale-110 duration-500 rounded-3xl">
+              <a href={item.href} target="_blank" rel="noreferrer">
+                <img className="w-20 mx-auto" src={item.src} alt={item.alt} />
+                <p className="my-4">{item.name}</p>
+              </a>
+            </div>
+          ))}
         </div>
       </div>
     </section>
